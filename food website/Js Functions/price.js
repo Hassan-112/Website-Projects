d831c7bb1f071f85753b29b222d2065265a159c3
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2022-12-13 00:27:34 +0500

    Selected card with js

---
+++ food website/Js Functions/price.js
@@ -0,0 +1,19 @@
+console.log("how are you");
+console.log("how are you");
+console.log("how are you");
+
+// var card = document.querySelectorAll(".card");
+// !important note 
+    // place your js file at the end of document for proper work some time you 
+    // may be not abbl to get acess of element 
+
+var card2 = document.getElementsByClassName('card');
+var ca = document.querySelector("#C");
+   
+
+
+    console.log(ca);
+// console.log(card2);
+// card2.addEventListener("click", (e)=>{
+//     alert("you clicked");
+// });
