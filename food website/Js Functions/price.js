d5d334c6ac68551fb2ba2eb00c1ffb37adf3bd45
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2022-12-13 00:52:39 +0500

    Add function from web but not working
    i wil add my own tomorrow

--- food website/Js Functions/price.js
+++ food website/Js Functions/price.js
@@ -10,8 +10,32 @@ console.log("how are you");
 var card2 = document.getElementsByClassName('card');
 var ca = document.querySelector("#C");
    
+ca.addEventListener("click",(e)=>{
+    // alert("you clicked")
+    if(e.target.tagName == "BUTTON"){
+        console.log("btn");
+    }
+    let card = e.target.parentNode;
+    console.log(e.target.tagName);
+})
 
-
+function testJS() {
+    var b = document.getElementById('name').value,
+        url = 'http://Accounts/Website-Projects/food website/deals.html?name=' + encodeURIComponent(b);
+        // H:http://Accounts/Website-Projects/food website/deals.html
+    document.location.href = url;
+}
+//use the following code where you want to receive data
+window.onload = function () {
+    var url = document.location.href,
+        params = url.split('?')[1].split('&'),
+        data = {}, tmp;
+    for (var i = 0, l = params.length; i < l; i++) {
+         tmp = params[i].split('=');
+         data[tmp[0]] = tmp[1];
+    }
+    document.getElementById('here').innerHTML = data.name;
+}
     console.log(ca);
 // console.log(card2);
 // card2.addEventListener("click", (e)=>{
